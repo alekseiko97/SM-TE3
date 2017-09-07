23f0253cceb12ec43c7415bf639e05d8a1bf4be6
Moskaljuk, Aleksei A <alekseiko@gmail.com>
2017-09-07 22:39:15 +0200

    finished with Group class

--- Assignment_week1.playground/Contents.swift
+++ Assignment_week1.playground/Contents.swift
@@ -64,53 +64,125 @@ class Lecturer : Person
     }
 }
 
- class Group
+public class Group
 {
-    var studentList = [Student]()
-    var lecturerList = [Lecturer]()
+    var groupName : Character
+    var studentList : [Student]
+    var lecturer : Lecturer?
     
-    init()
+    init(groupName: Character, lecuturer: Lecturer?)
     {
-        
+        self.groupName = groupName
+        self.lecturer = lecuturer
+        self.studentList = []
     }
     
-    func AddStudentToTheGroup(newStudent: Student)
-    {
-        studentList.append(newStudent)
-    }
-    
-    func AddLecturerToTheGroup(newLecturer: Lecturer)
-    {
-        lecturerList.append(newLecturer)
-    }
     func GetNrOfAllStudents() -> Int
     {
         return self.studentList.count
     }
-    func GetNrOfAllLecrurers() -> Int
-    {
-        return self.lecturerList.count
-    }
-
     
+    func GetStudent(studentNr: Int) -> Student?
+    {
+        for st in studentList
+        {
+            if st.studentNr == studentNr
+            {
+                return st
+            }
+        }
+        return nil
+    }
+    
+    func AddStudentToTheGroup(newStudent: Student)
+    {
+        for st in studentList
+        {
+            if newStudent.studentNr == st.studentNr
+            {
+                 print("This student already exists in this group")
+                 studentList.removeLast() // Removes the last added student that was already in the group
+                
+            }
+            
+        }
+        studentList.append(newStudent)
+    }
+    
+    /* func AddStudent(firstName: String, lastName: String, pcn: Int, studentNr: Int, profile: Profile)
+    {
+        if (GetStudent(studentNr: studentNr) == nil)
+        {
+            let s = Student(firstName: firstName, lastName: lastName, pcn: pcn, studentNr: studentNr, profile: profile)
+            studentList.append(s)
+        }
+        else
+        {
+            print("Student with \(studentNr) already exists")
+        }
+    } */
+    
+    func AddNewLecturerToTheGroup(newLecturer: Lecturer)
+    {
+        self.lecturer = newLecturer
+    }
+    func RemoveLecturerFromTheGroup()
+    {
+        self.lecturer = nil
+        print("No teacher is assigned to the group")
+    }
+    
+    func RemoveStudentFromTheGroup(student: Student)
+    {
+        var counter = 0
+        for st in studentList
+        {
+            
+            if (st.studentNr == student.studentNr)
+            {
+                
+                studentList.remove(at: counter)
+                
+            }
+            counter+=1
+        }
+    }
     
 }
 
 class Registration
 {
-    
+    var regStudents : [Student]
+    init()
+    {
+        self.regStudents = []
+    }
+    func RegisterStudent(student: Student)
+    {
+        
+    }
 }
 
+
+var lecturer1 = Lecturer(firstName: "John", lastName: "Lennon", pcn: 123456, salary: 750.00, function: Function.INTERNSHIP_COORDINATOR)
+var group1 = Group(groupName: "A", lecuturer: lecturer1)
+var lecturer2 = Lecturer(firstName: "Donald", lastName: "Trump", pcn: 234123, salary: 10000.00, function: Function.DIRECTOR)
 var student1 = Student(firstName: "Aleksei", lastName: "M", pcn: 360992, studentNr: 3008908, profile: Profile.SOFTWARE)
 var student2 = Student(firstName: "Bob", lastName: "Marley", pcn: 345934, studentNr: 532123, profile: Profile.BUSINESS)
-var lecturer1 = Lecturer(firstName: "John", lastName: "Lennon", pcn: 123456, salary: 750.00, function: Function.INTERNSHIP_COORDINATOR)
 
-var group = Group()
+group1.AddStudentToTheGroup(newStudent: student1)
+group1.AddStudentToTheGroup(newStudent: student2)
+group1.RemoveStudentFromTheGroup(student: student2)
+print(group1.GetNrOfAllStudents())
+print(group1.lecturer?.firstName)
+group1.AddNewLecturerToTheGroup(newLecturer: lecturer2)
+print(group1.lecturer?.firstName)
+group1.RemoveLecturerFromTheGroup()
+print(group1.lecturer?.firstName)
 
-group.AddStudentToTheGroup(newStudent: student1)
-group.AddStudentToTheGroup(newStudent: student2)
-group.AddLecturerToTheGroup(newLecturer: lecturer1)
 
-print(group.GetNrOfAllStudents() + group.GetNrOfAllLecrurers())
+
+
+
 
 
