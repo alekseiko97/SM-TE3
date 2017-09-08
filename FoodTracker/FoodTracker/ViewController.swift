ddfc4f0e18162e9d35477f46fb29b3d5d3974473
Moskaljuk, Aleksei A <alekseiko@gmail.com>
2017-09-08 12:31:48 +0200

    FoodTracker app UI
    week2 assignment

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Fhict on 08/09/2017.
+//  Copyright © 2017 Fhict. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
